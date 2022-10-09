e24d0c6f10ca7f73667c918ad86ef56a00a6a4d7
relero90 <relero90@gmail.com>
2022-10-09 13:48:05 -0600

    pseudo-coded About.js component

--- professional-portfolio/src/components/About.js
+++ professional-portfolio/src/components/About.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
 function About() {
+  // Components:
+  // Quick Bio / Brand statement
+  // Languages / Technologies section w/ icons
+  // HTML5, Bootstrap, JS, Node, NPM, MySQL/WorkBench, MongoDB/Compass, Handlebars, ReactJS
+
   return <div className="text-light">About Me Coming Soon</div>;
 }
 
