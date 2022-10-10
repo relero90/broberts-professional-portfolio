c887d388a8d80f3236b90ac9b3980b3501d1faaf
relero90 <relero90@gmail.com>
2022-10-09 22:33:36 -0600

    Updates

---
+++ precache-manifest.1d4b79f2d96b3dabb4c72093d9b2b4d1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc1ec425ed203e54c1f2",
+    "url": "/professional-portfolio-site/static/css/main.fcf1c75c.chunk.css"
+  },
+  {
+    "revision": "fc1ec425ed203e54c1f2",
+    "url": "/professional-portfolio-site/static/js/main.fc1ec425.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/professional-portfolio-site/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "a0d0806ff1d3ab6e7ff4",
+    "url": "/professional-portfolio-site/static/js/2.a0d0806f.chunk.js"
+  },
+  {
+    "revision": "cc22001402bb0555c57e",
+    "url": "/professional-portfolio-site/static/js/runtime~main.cc220014.js"
+  },
+  {
+    "revision": "73b5d4decfcbc8c0a544eb695c04d885",
+    "url": "/professional-portfolio-site/static/media/Resume.73b5d4de.pdf"
+  },
+  {
+    "revision": "37327173b11305c5869e93dc87f4a1a0",
+    "url": "/professional-portfolio-site/static/media/headshot.37327173.jpeg"
+  },
+  {
+    "revision": "76067ae06b75c09be7705ea73bc36184",
+    "url": "/professional-portfolio-site/static/media/brewfinder.76067ae0.png"
+  },
+  {
+    "revision": "a5a44f5ba4496adf8b63e51d8f447bc1",
+    "url": "/professional-portfolio-site/static/media/kiss-the-cook.a5a44f5b.png"
+  },
+  {
+    "revision": "1058149675fa57b4ee5f288d16ca7c5b",
+    "url": "/professional-portfolio-site/static/media/mysqlEmployeeTracker.10581496.png"
+  },
+  {
+    "revision": "1199cdc6e79a3d826bb7393772297ab5",
+    "url": "/professional-portfolio-site/static/media/weatherApp.1199cdc6.png"
+  },
+  {
+    "revision": "c26b6307e8472fb712a43f1be8627856",
+    "url": "/professional-portfolio-site/static/media/READMEgen.c26b6307.png"
+  },
+  {
+    "revision": "6676c7ca16d1d80ee8baecb45f0e1f3e",
+    "url": "/professional-portfolio-site/index.html"
+  }
+];
