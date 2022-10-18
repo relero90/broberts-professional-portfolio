4b33c19a3de23a9be1abfcb183b76a6fd618bfd9
relero90 <relero90@gmail.com>
2022-10-18 15:25:47 -0600

    resume download page styling

--- professional-portfolio/src/pages/resume/Resume.js
+++ professional-portfolio/src/pages/resume/Resume.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import "./Resume.css";
 import resume from "./Resume.pdf";
+import resumeClip from "./Resume.jpeg";
 
 function Resume() {
   return (
     <div className="resumeDownload">
-      <h3 className="resumeTitle">Resume</h3>
-      <a title="Download Resume" href={resume} download>
-        <button className="btn downloadBtn">Click to Download</button>
-      </a>
+      <div className="resumeCard">
+        <h3 className="resumeTitle">Resume</h3>
+        <img src={resumeClip} alt="Becca's resume" className="resumeImg"></img>
+        <a
+          title="Download Resume"
+          href={resume}
+          download
+          className="btn downloadBtn"
+        >
+          Click to Download
+        </a>
+      </div>
     </div>
   );
 }
